b69f301eea004828cabd5604d8130f048ff9ce68
Siarhei Yafimenka <h1_n1@mail.ru>
2023-05-03 23:49:12 +0300

    feat: add solution 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
+const directory = path.join(__dirname, 'secret-folder');
+
+fs.promises.readdir(directory, { withFileTypes: true }).then((files) => {
+  files.filter(el => el.isFile())
+    .forEach(el => {
+      const fileName = path.parse(el.name).name;
+      const fileExt = path.parse(el.name).ext;
+      fs.stat(path.join(directory, el.name), (err, stats) => {
+        console.log(fileName + fileExt.replace('.', ' - ') + ' - ' + stats.size)
+      });
+    })
+})
