ae501b7c16ec16d486a551b2395f035ca08a3964
Siarhei Yafimenka <h1_n1@mail.ru>
2023-05-04 18:45:30 +0300

    feat: add solution 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,39 @@
+const fs = require('fs');
+const { join } = require('path');
 
+(function makeDirectory() {
+  const filesCopyDir = join(__dirname, 'files-copy');
+
+  fs.access(filesCopyDir, fs.constants.F_OK, (err) => {
+    if (err) {
+      fs.promises.mkdir(filesCopyDir, { recursive: true });
+      copyDir();
+    }
+    else {
+      clearDir(filesCopyDir);
+      copyDir();
+    }
+  })
+})();
+
+const clearDir = (dir) => {
+  fs.promises.readdir(dir, { withFileTypes: true }).then((files) => {
+    files.forEach(el => {
+      fs.unlink(join(__dirname, 'files-copy', el.name), err => {
+        if (err) throw err;
+      });
+    })
+  });
+}
+
+const copyDir = () => {
+  const filesDir = join(__dirname, 'files');
+  fs.promises.readdir(filesDir, { withFileTypes: true }).then((files) => {
+    files.forEach(el => {
+      const file = join(__dirname, 'files', el.name);
+      const copyFile = join(__dirname, 'files-copy', el.name);
+
+      fs.promises.copyFile(file, copyFile);
+    })
+  })
+}
